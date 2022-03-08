fb1e33bd9cddadcddd4d6bce82ad6bec6b5084ea
laurammorais <laulaumorais@gmail.com>
2022-03-08 13:40:26 -0300

    Adicionar arquivos de projeto.

---
+++ ex 3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ex_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			int num = 0, par = 0, impar = 0;
+
+			while (true)
+			{ 
+				Console.Write("Digite um numero ate 1000: ");
+				num = int.Parse(Console.ReadLine());
+				
+				if (num > 1000)
+                {
+					break;
+                } 
+				else if (num % 2 == 0)
+				{
+					par = par + num;
+				}
+				else
+				{
+					impar = impar + num;
+				}
+			}
+
+			Console.WriteLine("A soma dos numeros pares é: {0}", par);
+			Console.WriteLine("A soma dos numeros impares é: {0}", impar);
+		}
+    }
+}
